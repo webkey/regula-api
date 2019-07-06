8b737d39d45908e79d55bd9b0a63e8af98604d54
webkey <serhiy_i@ukr.net>
2019-07-07 02:08:49 +0300

    Шапка, футер, нижние текстовые блоки

--- gulpfile.js
+++ gulpfile.js
@@ -284,7 +284,7 @@ gulp.task('buildDist', ['cleanDist', 'html:buildAllPages', 'copyImgToDist', 'sas
   gulp.src('src/assets/**/*')
       .pipe(gulp.dest(path.dist + '/assets'));
 
-  gulp.src(['!src/__*.html', '!src/forms.html', '!src/_tpl_*.html', '!src/_temp_*.html', 'src/*.html'])
+  gulp.src(['!src/__*.html', '!src/forms.html', '!src/typography.html', '!src/_tpl_*.html', '!src/_temp_*.html', 'src/*.html'])
       .pipe(revts()) // Добавить версии подключаемых файлов. В html добавить ключ ?rev=@@hash в место добавления версии
       .pipe(gulp.dest(path.dist));
 
